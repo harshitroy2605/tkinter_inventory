7c8834066ceac53ac3899f9c962c600cbc92da9d
Harshit Roy <harshitroy2605@users.noreply.github.com>
2021-02-12 12:03:37 +0530

    added remove label on change function feature

--- frontend.py
+++ frontend.py
@@ -22,7 +22,20 @@ OPTIONS = [
 
 list_of_inserted_chategory=[]
 
+previous_choice_made=0
 
+#----------------------------------
+
+product_name_label=0
+product_name=0
+product_quantity=0
+product_quantity_label=0
+product_category_label=0
+product_category_n=0
+add_more_category_button=0
+final_insertion_button=0
+add_more_category_button=0
+clear_all_button=0
 
 #----------------------initial gui parts -------------------------#
 
@@ -86,7 +99,37 @@ def price_reminder(quantity):
 	print(quantity)
 
 
+def delete_previous_label(choice):
+	print(previous_choice_made)
+	if previous_choice_made=="New Product":
+		product_name_label.place_forget()
+		product_name.place_forget()
+		product_quantity.place_forget()
+		product_quantity_label.place_forget()
+		product_category_label.place_forget()
+		product_category_n.place_forget()
+		add_more_category_button.place_forget()
+		final_insertion_button.place_forget()
 
+
+	elif previous_choice_made=="Search":
+		product_name_label.place_forget()
+		product_name.place_forget()
+		product_quantity.place_forget()
+		product_quantity_label.place_forget()
+		product_category_label.place_forget()
+		product_category_n.place_forget()
+		final_insertion_button.place_forget()
+
+	elif previous_choice_made=="update stock":
+		product_name_label.place_forget()
+		product_name.place_forget()
+		product_quantity.place_forget()
+		product_quantity_label.place_forget()
+		product_category_label.place_forget()
+		product_category_n.place_forget()
+		add_more_category_button.place_forget()
+		final_insertion_button.place_forget()
 
 
 
@@ -121,8 +164,10 @@ what_to_do_choice.place(x=390,y=202)
 
 
 def option_gui():
+	global previous_choice_made,product_name_label,product_name,product_quantity,product_quantity_label,product_category_label,product_category_n,add_more_category_button,final_insertion_button,add_more_category_button,clear_all_button
 	action_want_to_perform=choice.get()
 	if choice.get()=="New Product":
+		delete_previous_label(previous_choice_made)
 
 		product_name_label=Label(window,text="product name",font=("times",18,"bold"))
 		product_name_label.place(x=30,y=300)
@@ -136,30 +181,35 @@ def option_gui():
 		product_quantity=Text(window,height=1,width=14)
 		product_quantity.place(x=390,y=350)
 		quantity=product_quantity.get(1.0,END+"-1c")
+
+		previous_choice_made="New Product"
+
+
+
+		product_category_label=Label(window,text="Enter Product category",font=("times",18,"bold"))
+		product_category_label.place(x=30,y=400)
+
+
+		product_category_n=Text(window,height=1,width=14)
+		product_category_n.place(x=390,y=400)
+
+
+		add_more_category_button=Button(window,text="add",bg = "white",width=5,command=lambda:[add_more_category_function(product_category_n.get(1.0,END+"-1c")),product_category_n.delete('1.0',END)])
+		add_more_category_button.place(x=520,y=400)
 
 
 		final_insertion_button=Button(window,text="submit",bg = "white",width=10,command=lambda:[insert_entry(product_name.get(1.0,END+"-1c"),product_quantity.get(1.0,END+"-1c")),product_name.delete('1.0',END),product_quantity.delete('1.0',END)])
 		final_insertion_button.place(x=200,y=450)
 
 
-		product_category_label=Label(window,text="Add Product category ",font=("times",18,"bold"))
-		product_category_label.place(x=30,y=400)
 
-
-		product_category_n=Text(window,height=1,width=14)
-		product_category_n.place(x=390,y=400)
-
-
-		add_more_category_button=Button(window,text="add",bg = "white",width=5,command=lambda:[add_more_category_function(product_category_n.get(1.0,END+"-1c")),product_category_n.delete('1.0',END)])
-		add_more_category_button.place(x=520,y=400)
-
-
-		clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_quantity.place_forget(),product_quantity_label.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),add_more_category_button.place_forget(),final_insertion_button.place_forget(),add_more_category_button.place_forget(),clear_all_button.place_forget()])
-		clear_all_button.place(x=340,y=448)
+		#clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_quantity.place_forget(),product_quantity_label.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),add_more_category_button.place_forget(),final_insertion_button.place_forget(),add_more_category_button.place_forget(),clear_all_button.place_forget()])
+		#clear_all_button.place(x=340,y=448)
 
 
 
 	elif choice.get()=="Search":
+		delete_previous_label(previous_choice_made)
 		product_name_label=Label(window,text="product name",font=("times",18,"bold"))
 		product_name_label.place(x=30,y=300)
 		
@@ -167,21 +217,29 @@ def option_gui():
 		product_name.place(x=390,y=300)
 
 
-		product_category_label=Label(window,text="Enter any Product category ",font=("times",18,"bold"))
-		product_category_label.place(x=30,y=350)
+		product_category_label=Label(window,text="Enter Product category",font=("times",18,"bold"))
+		product_category_label.place(x=30,y=400)
 
 
 		product_category_n=Text(window,height=1,width=14)
-		product_category_n.place(x=390,y=350)
+		product_category_n.place(x=390,y=400)
 
+		product_quantity_label=Label(window,text="Quantity",font=("times",18,"bold"))
+		product_quantity_label.place(x=30,y=350)
 
-		final_insertion_button=Button(window,text="submit",bg = "white",width=10,command=lambda:search_entry(product_name.get(1.0,END+"-1c"),product_category_n.get(1.0,END+"-1c")))
+		product_quantity=Text(window,height=1,width=14)
+		product_quantity.place(x=390,y=350)
+		quantity=product_quantity.get(1.0,END+"-1c")
+
+		previous_choice_made="search"
+		final_insertion_button=Button(window,text="Search",bg = "white",width=10,command=lambda:search_entry(product_name.get(1.0,END+"-1c"),product_category_n.get(1.0,END+"-1c")))
 		final_insertion_button.place(x=200,y=450)
 
-		clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),final_insertion_button.place_forget(),clear_all_button.place_forget()])
-		clear_all_button.place(x=340,y=448)
+		#clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),final_insertion_button.place_forget(),clear_all_button.place_forget()])
+		#clear_all_button.place(x=340,y=448)
 
 	elif choice.get()=="update stock":
+		delete_previous_label(previous_choice_made)
 		product_name_label=Label(window,text="product name",font=("times",18,"bold"))
 		product_name_label.place(x=30,y=300)
 
@@ -197,24 +255,26 @@ def option_gui():
 		quantity=product_quantity.get(1.0,END+"-1c")
 
 
-		final_insertion_button=Button(window,text="submit",bg = "white",width=10,command=lambda:)
-		final_insertion_button.place(x=200,y=450)
+		#final_insertion_button=Button(window,text="submit",bg = "white",width=10,command=lambda:)
+		#final_insertion_button.place(x=200,y=450)
 
 
-		product_category_label=Label(window,text="Add Product category ",font=("times",18,"bold"))
+		product_category_label=Label(window,text="Enter Product category",font=("times",18,"bold"))
 		product_category_label.place(x=30,y=400)
 
 
 		product_category_n=Text(window,height=1,width=14)
 		product_category_n.place(x=390,y=400)
 
+		previous_choice_made="update stock"
+
 
 		add_more_category_button=Button(window,text="add",bg = "white",width=5,command=lambda:[add_more_category_function(product_category_n.get(1.0,END+"-1c")),product_category_n.delete('1.0',END)])
 		add_more_category_button.place(x=520,y=400)
 
 
-		clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_quantity.place_forget(),product_quantity_label.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),add_more_category_button.place_forget(),final_insertion_button.place_forget(),add_more_category_button.place_forget(),clear_all_button.place_forget()])
-		clear_all_button.place(x=340,y=448)
+		#clear_all_button=Button(window,text="remove all",bg = "white",width=15,command=lambda:[product_name_label.place_forget(),product_name.place_forget(),product_quantity.place_forget(),product_quantity_label.place_forget(),product_category_label.place_forget(),product_category_n.place_forget(),add_more_category_button.place_forget(),final_insertion_button.place_forget(),add_more_category_button.place_forget(),clear_all_button.place_forget()])
+		#clear_all_button.place(x=340,y=448)
 
 
  
