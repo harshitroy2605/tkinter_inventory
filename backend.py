41b46da2dcb5e7f327e04dda527bab86cc9896af
Harshit Roy <harshitroy2605@users.noreply.github.com>
2021-02-13 07:40:20 +0530

    added popup for product detail

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 import json
 import sqlite3
+from tkinter import messagebox
 
 def connect():
     conn=sqlite3.connect("database.db")
@@ -17,7 +18,31 @@ def insert(name,quantity,chategories):
     conn.close()
     view()
 
+def display_list(name=""):
+    #print(name)
+    conn=sqlite3.connect("database.db")
+    cur=conn.cursor()
+    cur.execute("SELECT DISTINCT chategory FROM products WHERE name=?",(name,))
+    row=cur.fetchall()
+    chategory=str(row)
+    #print(type(row))
+    chategory=chategory.replace('[','')
+    chategory=chategory.replace('(','')
+    chategory=chategory.replace("'",'')
+    chategory=chategory.replace(',','')
+    chategory=chategory.replace(']','')
+    chategory=chategory.replace(')','')
 
+    print(chategory)
+    #chategory=''.join(e for e in chategory if e.isalnum())
+    cur.execute("SELECT DISTINCT quantity FROM products WHERE name=?",(name,))
+    rows=cur.fetchall()
+    quantity=str(rows)
+    quantity=''.join(e for e in quantity if e.isalnum())
+
+
+    conn.close()
+    messagebox.showinfo("showinfo",'name : '+name+'\n'+'quantity : '+quantity+'\n'+'chategory : '+chategory)
 
 def search(name="",chategory=""):
     conn=sqlite3.connect("database.db")
@@ -29,15 +54,26 @@ def search(name="",chategory=""):
 
 
 
+def update(name,quantity,chategory,chategory_list):
+    conn=sqlite3.connect("database.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE products SET name=? , quantity=? where name=?",(name,quantity,name))
+    if chategory_list is not None:
+        for i in chategory_list:
+            cur.execute("INSERT INTO products VALUES (NULL, ?,?,?)",(name,quantity,i))
+    conn.commit()
+    conn.close()
+
+
 
 
 def view():
     view_result=[]
     conn=sqlite3.connect("database.db")
     cur=conn.cursor()
-    cur.execute("SELECT name , quantity FROM products GROUP BY name ")
+    cur.execute("SELECT name FROM products GROUP BY name ")
     row=cur.fetchall() 
-    print(row)
+    #print(row)
     #print(type(row))
     '''for i in row:
         cur.execute("SELECT chategory FROM products WHERE name==?",(i))
